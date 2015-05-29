2fa09ac5926494d1698486eac79a812baebad104
Fabián <fabianjim1995@gmail.com>
2015-05-28 22:13:30 -0500

    Solved exercises of javascripting by NodeSchool

---
+++ scope.js
@@ -0,0 +1,24 @@
+var a = 1, b = 8, c = 6;
+
+(function firstFunction()
+{
+    var b = 8, c = 6;
+
+    (function secondFunction()
+	{
+        var b = 8;
+
+        (function thirdFunction()
+		{
+            var a = 1, c = 9;
+
+            (function fourthFunction()
+			{
+                var a = 1, c = 6;
+
+            })();
+        })();
+    })();
+})();
+
+console.log("a: " + a + ", b: " + b + ",c: " + c);
